7c275393fa53bf7ae647b4178f2b828d719ee469
n-pizzetta <154426149+n-pizzetta@users.noreply.github.com>
2024-05-30 10:32:14 +0200

    Update

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def setup_java():
         st.error("JAVA_HOME is not set.")
         return None
 
-
 # Fonction pour mettre à jour la barre de progression
 def update_progress(progress):
     current_progress = (i + progress) / total_files
@@ -177,77 +176,90 @@ st.title("Application de conversion et concaténation de fichiers")
 # Choix du mode d'utilisation
 mode = st.selectbox("Choisissez une option", ["Conversion de fichiers Access en CSV", "Concaténation de fichiers CSV/Excel"])
 
-uploaded_files = None
 
 if mode == "Conversion de fichiers Access en CSV":
     st.header("Conversion de fichiers Access en CSV")
+
+    uploaded_files = None
     uploaded_files = st.file_uploader("Choisissez des fichiers .accdb en ne dépassant pas les 400MB", type="accdb", accept_multiple_files=True)
 
-    if uploaded_files:
-
-        if 'converted_files' not in st.session_state:
-            st.session_state.converted_files = {}
+    @st.experimental_fragment
+    def convert_files(uploaded_file):
         
-        # Vérifier et nettoyer les fichiers si de nouveaux fichiers sont uploadés avant téléchargement
-        if st.session_state.get('converted_files', {}):
-            clear_converted_files()
+        if uploaded_files:
 
-        # Obtenir le répertoire de travail courant
-        current_dir = os.getcwd()
+            if 'converted_files' not in st.session_state:
+                st.session_state.converted_files = {}
+            
+            # Vérifier et nettoyer les fichiers si de nouveaux fichiers sont uploadés avant téléchargement
+            if st.session_state.get('converted_files', {}):
+                clear_converted_files()
 
-        # Afficher le répertoire courant pour le débogage
-        #st.write(f"Current directory: {current_dir}")
-        #st.write(f"Fichiers présents dans le répertoire : {os.listdir(current_dir)}")
+            # Obtenir le répertoire de travail courant
+            current_dir = os.getcwd()
 
-        # Liste des fichiers JAR nécessaires pour UCanAccess
-        ucanaccess_jars = [
-            'ucanaccess-5.0.1.jar',
-            os.path.join('loader', 'ucanload.jar'),
-            os.path.join('lib', 'commons-lang3-3.8.1.jar'),
-            os.path.join('lib', 'commons-logging-1.2.jar'),
-            os.path.join('lib', 'hsqldb-2.5.0.jar'),
-            os.path.join('lib', 'jackcess-3.0.1.jar')
-        ]
+            # Afficher le répertoire courant pour le débogage
+            #st.write(f"Current directory: {current_dir}")
+            #st.write(f"Fichiers présents dans le répertoire : {os.listdir(current_dir)}")
 
-        # Concaténer les chemins des fichiers JAR correctement
-        classpath = ":".join([os.path.join(current_dir, "UCanAccess-5.0.1.bin", jar) for jar in ucanaccess_jars])
+            # Liste des fichiers JAR nécessaires pour UCanAccess
+            ucanaccess_jars = [
+                'ucanaccess-5.0.1.jar',
+                os.path.join('loader', 'ucanload.jar'),
+                os.path.join('lib', 'commons-lang3-3.8.1.jar'),
+                os.path.join('lib', 'commons-logging-1.2.jar'),
+                os.path.join('lib', 'hsqldb-2.5.0.jar'),
+                os.path.join('lib', 'jackcess-3.0.1.jar')
+            ]
 
-        # Afficher le classpath pour le débogage
-        #st.write(f"Classpath: {classpath}")
+            # Concaténer les chemins des fichiers JAR correctement
+            classpath = ":".join([os.path.join(current_dir, "UCanAccess-5.0.1.bin", jar) for jar in ucanaccess_jars])
 
-        if not jpype.isJVMStarted():
-            #st.write("Starting JVM...")
-            jpype.startJVM(
-                jpype.getDefaultJVMPath(),
-                *['-Xms512m', '-Xmx2048m'],     # Augmentationd de la mémoire allouée à la JVM
-                #classpath = "-Djava.class.path=" + classpath
-                classpath = classpath
-                )
-            #st.write(f"JVM started successfully")
+            # Afficher le classpath pour le débogage
+            #st.write(f"Classpath: {classpath}")
+
+            if not jpype.isJVMStarted():
+                #st.write("Starting JVM...")
+                jpype.startJVM(
+                    jpype.getDefaultJVMPath(),
+                    *['-Xms512m', '-Xmx2048m'],     # Augmentationd de la mémoire allouée à la JVM
+                    #classpath = "-Djava.class.path=" + classpath
+                    classpath = classpath
+                    )
+                #st.write(f"JVM started successfully")
+            
+            progress_bar = st.progress(0)
+            status_text = st.empty()
+            total_files = len(uploaded_files)
+
+            for i, uploaded_file in enumerate(uploaded_files):
+                file_name = uploaded_file.name.split('.')[0]
+                if file_name not in st.session_state.converted_files:
+                    # Créer un fichier temporaire pour l'upload
+                    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                        tmp_file.write(uploaded_file.getbuffer())
+                        tmp_file_path = tmp_file.name
+
+                    # Lire les données du fichier Access
+                    data = read_access_file(tmp_file_path, ucanaccess_jars, update_progress)
+                    # Sauvegarder les résultats intermédiaires
+                    csv_data, file_name = save_to_csv(data, f"{uploaded_file.name.split('.')[0]}.csv")
+                    st.session_state.converted_files[file_name] = csv_data
+
+                    # Supprimer le fichier temporaire après traitement
+                    os.remove(tmp_file_path)
+
+            # Créer un fichier ZIP contenant tous les fichiers CSV convertis
+            create_zip_file(st.session_state.converted_files)
         
-        progress_bar = st.progress(0)
-        status_text = st.empty()
-        total_files = len(uploaded_files)
-
-        for i, uploaded_file in enumerate(uploaded_files):
-            file_name = uploaded_file.name.split('.')[0]
-            if file_name not in st.session_state.converted_files:
-                # Créer un fichier temporaire pour l'upload
-                with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                    tmp_file.write(uploaded_file.getbuffer())
-                    tmp_file_path = tmp_file.name
-
-                # Lire les données du fichier Access
-                data = read_access_file(tmp_file_path, ucanaccess_jars, update_progress)
-                # Sauvegarder les résultats intermédiaires
-                csv_data, file_name = save_to_csv(data, f"{uploaded_file.name.split('.')[0]}.csv")
-                st.session_state.converted_files[file_name] = csv_data
-
-                # Supprimer le fichier temporaire après traitement
-                os.remove(tmp_file_path)
-
-        # Créer un fichier ZIP contenant tous les fichiers CSV convertis
-        create_zip_file(st.session_state.converted_files)
+        # Créer un bouton pour télécharger le fichier ZIP
+        st.download_button(
+            label="Convertir les fichiers",
+            data=uploaded_files,
+            file_name="converted_files.zip",
+            mime="application/zip",
+            on_click=convert_files
+            )
 
         # Mise à jour de la barre de progression et du message
         progress_bar.progress(1.0)
